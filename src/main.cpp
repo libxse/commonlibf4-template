0859bf059af1e03ca40a5f1a71828256d7a46023
Qudix <17361645+Qudix@users.noreply.github.com>
2026-08-22 09:42:19 -0500

    feat: update lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
+F4SE_PLUGIN_PRELOAD(const F4SE::PreLoadInterface* a_f4se)
+{
+	F4SE::Init(a_f4se);
+
+	REX::INFO("Hello PreLoaded World!");
+
+	return true;
+}
+
 F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
 	F4SE::Init(a_f4se);
 
-	REX::INFO("Hello World!");
+	REX::INFO("Hello Loaded World!");
 
 	return true;
 }
